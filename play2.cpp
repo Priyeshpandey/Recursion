837348d3c5f930e03b3c68c55a9ca3beadd8f900
Priyeshpandey <pandeypriyesh184@gmail.com>
2016-08-17 12:03:28 +0530

    Update play2.cpp

--- play2.cpp
+++ play2.cpp
@@ -33,8 +33,56 @@ class fibo{
                     }
 
 };
+//Algorithm using matrices both time efficient and storage efficient. Time complexity O(log(n)).
+    double cat[2][2];
+double base2[2][2]={{1,1},{1,0}}; double init[2][1]={{1},{0}},init2[2][1]={{1},{0}};   /*base2 is the fixed array. init2-->initial array.
+                                                                                init-->final array to store the result*/
+double fib2(double base[2][2],int y){             /*base is iterative array, y is the no. of times recursive call is to be made.
+                                                Also the argument for evaluating fibonacci number*/
+    int i,j,k;
+    --y;
+    if(y>0){
+        for(i=0;i<2;i++){
+            for(j=0;j<2;j++){
+                    cat[i][j]=0;
+                for(k=0;k<2;k++){
+                    cat[i][j]+=base2[i][k]*base[k][j];
+                }
+                //cout<<cat[i][j]<<" "; //For debugging
+            }
+            //cout<<endl; //For debugging.
+        }
+        for(i=0;i<2;i++){
+            for(j=0;j<2;j++){
+                base[i][j]=cat[i][j];
+            }
+        }
+        return fib2(base,y);
+    }
+    else
+        {
+            for(i=0;i<2;i++){
+            for(j=0;j<1;j++){
+                    init[i][j]=0;
+                for(k=0;k<2;k++){
+                    init[i][j]+=base[i][k]*init2[k][j];
+                }
+            }
+        }
+        return init[1][0];  //Stores Fibonacci(y);
+        }
+
+}
+
 
 int main(){
-    fibo s;
-    cout<<setprecision(21)<<s.fibonacci(30);
+   // fibo s;
+   // cout<<setprecision(21)<<s.fibonacci(30);  //Uses 1st method with linear time complexity
+    int y;
+    double base[2][2]={{1,1},{1,0}};
+    cout<<"Enter The argument Value:\n";
+    cin>>y;
+    double r=fib2(base,y);          //Uses 2nd Method with logarithmic time complexity.
+    cout<<setprecision(210)<<r; //Shows 210 digits at max.
+    getch();
 }
